7a0b1a0eb8f91da26f3597a512b3725df26a7ee0
AnjanTH <anjanth2552004@gmail.com>
2024-11-18 08:32:34 +0530

    food recipe

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PageHome from "./Pages/Home/PageHome";
 import Register from "./Pages/AuthPages/Register";
-import Login from "./Pages/AuthPages/Login"
-
+import Login from "./Pages/AuthPages/Login";
+import Dashboard from "./Pages/Dashboard.jsx/Dashboard";
+import RecipeGenerator from "./Pages/Foodrecipe/FoodRecipes";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<PageHome />} />
-          <Route path="register" element={<Register/>}/>
-          <Route path="login" element={<Login/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/recipes" element={<RecipeGenerator/>}/>
         </Routes>
       </Router>
     </React.StrictMode>
